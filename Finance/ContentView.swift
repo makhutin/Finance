ab7de37511d9575cabc9a0bbc242e65df4da5400
Mahutin Aleksej <mahutin@me.com>
2019-10-07 10:06:26 +0300

    Initial Commit

---
+++ Finance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance
+//
+//  Created by Mahutin Aleksei on 07/10/2019.
+//  Copyright © 2019 Mahutin Aleksei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
